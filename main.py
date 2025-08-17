260653f338c9cf135026276de227b39a1b0b3959
Shokuno <rul2ai@hotmail.com>
2025-08-18 00:18:01 +0200

    added character count feature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import character_count
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,6 +8,11 @@ def get_book_text(filepath):
 
 def main():
     frankenstein_text = get_book_text("books/frankenstein.txt")
+
+    char_count = character_count(frankenstein_text)
+
     print(get_num_words(frankenstein_text))
+    print(char_count)
+    
 
 main()
--- stats.py
+++ stats.py
@@ -5,3 +5,16 @@ def get_num_words(text):
     word_count = len(words)
     
     return f"{word_count} words found in the document"
+
+def character_count(text):
+    lower_text = text.lower()
+    
+    char_dictionary = {}
+
+    for char in lower_text:
+        if char in char_dictionary:
+            char_dictionary[char] = char_dictionary[char] + 1
+        else:
+            char_dictionary[char] = 1
+    
+    return char_dictionary
